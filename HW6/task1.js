343b0de90adf0cb371133b59e25df5579699039b
Elena Lubianska <elena.lubianska@MacBook-Air-uklon-admin-3.local>
2025-03-10 23:00:03 +0200

    add hw6

---
+++ HW6/task1.js
@@ -0,0 +1,16 @@
+// Function Declaration
+//function areaRectangle(width, height){
+//    return width * height
+//}
+//let S = areaRectangle (5, 10)
+//console.log("Площа прямокутника " + S)
+
+// Function Expression
+//let areaRectangle = function (width, height){
+//    return width * height
+//}
+//console.log("Площа прямокутника " + areaRectangle (5, 10))
+
+//Arrow Function
+let areaRectangle = (width, height) => width * height
+console.log("Площа прямокутника " + areaRectangle (5, 10))
---
+++ HW6/task2.js
@@ -0,0 +1,3 @@
+let isAdult = (age) => (age >= 18) ? true : false
+console.log("Are you adult?" + " "+ isAdult (5))
+console.log("Are you adult?" + " "+ isAdult (25))
