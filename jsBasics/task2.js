90f0742078b2e104d5907652f8c8aa0680434717
Elena Lubianska <elena.lubianska@MacBook-Air-uklon-admin-3.local>
2025-03-10 22:03:20 +0200

    add hw5

--- jsBasics/task2.js
+++
@@ -1,5 +0,0 @@
-let name1 = "Maria"
-let name2 = "Olga"
-let sentence = "Hello"
-console.log(sentence +" "+ name1 +" "+ "and" +" " + name2)
-console.log(`Hello ${name1}`)
--- jsBasics/task3.js
+++
@@ -1,4 +0,0 @@
-let age  = 14
-let adult = age>=18
-console.log(age)
-console.log("Are you adult?" + " "+ adult)
